1463c19b9328364e449f8c3f55fe9ec2f0beec1d
Mehtavarun <mehtavarun96@rediffmail.com>
2017-10-01 22:02:46 +0530

    Delete function added

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Button from 'material-ui/Button';
-//import {FloatingButton} from 'material-ui/FloatingActionButton';
-//import {ContentAdd} from 'material-ui/svg-icons/content/add';
+// import Button from 'material-ui/Button';
+import FloatingButton from 'material-ui/FloatingActionButton';
+import FlatButton from 'material-ui/FlatButton';
+import baseTheme from 'material-ui/styles/baseThemes/lightBaseTheme';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import ContentAdd from 'material-ui/svg-icons/content/add';
+import ActionDelete from 'material-ui/svg-icons/action/delete';
+import Edit from 'material-ui/svg-icons/image/edit';
 
 class App extends React.Component{
 
@@ -27,13 +32,23 @@ class App extends React.Component{
 		}));
 	}
 
+	deleteContacts(id){
+		this.state.contacts.splice(id,1);
+		this.setState({
+			contacts:this.state.contacts
+		});
+	}
+
+	getChildContext() {
+        return { muiTheme: getMuiTheme(baseTheme) };
+    }
+
 	render(){
 		return(
 			<div>
-				<h2>Contacts</h2>
-				<Button onClick = {this.showContacts.bind(this)}>New</Button>
+				<h1>Contacts &emsp;&emsp;&emsp;<FloatingButton onClick = {this.showContacts.bind(this)}> <ContentAdd/> </FloatingButton></h1>
 				{(this.state.showCt) ? <NewContact addContact={this.addContact.bind(this)}/> : null}
-				<ContactList contacts={this.state.contacts}/>
+				<ContactList contacts={this.state.contacts} deleteCnt={this.deleteContacts.bind(this)} addContact={this.addContact.bind(this)}/>
 			</div>
 		);
 	};
@@ -45,7 +60,10 @@ class ContactList extends React.Component{
 		super(props);
 		this.state={
 			show:[false],
-			id:-1
+			id:-1,
+			edit:[false],
+			editId:-1,
+			contacts:this.props.contacts
 		};
 	}
 
@@ -56,7 +74,7 @@ class ContactList extends React.Component{
 			e = false;
 		})
 
-		if(arr[id]===true || id==this.state.id){
+		if(id===this.state.id){
 			arr[id]=false
 		} else {
 			arr[id] = true
@@ -67,11 +85,34 @@ class ContactList extends React.Component{
 			id:id
 		}));
 
-		if(id==this.state.id){
+		if(id===this.state.id){
 			this.setState({
 				id:-1
 			})
 		}
+	}
+
+	deleteCnt(id){
+		this.props.deleteCnt(id);
+	}
+
+	editCnt(id){
+
+		var arr = new Array(this.props.contacts.length);
+		arr.map((e)=>{
+			e = false;
+		});
+
+		this.setState(prevState=>({
+			show: arr
+		}));
+
+		arr[id]=true;
+		this.setState(prevState=>({
+			editId: arr
+		}));
+
+		console.log(this.state.edit[id]);
 	}
 
 	render(){
@@ -79,14 +120,66 @@ class ContactList extends React.Component{
 			<div>
 				<ol>
 					{this.props.contacts.map((cont,i)=>
-						<li key={i}>
-							<Button key={i} onClick = {this.clickAct.bind(this,i)}>{cont.name}</Button><br/>
-							{(this.state.show[i])?<p>{cont.email}<br/>{cont.no}</p>:null}
+						<li key={i}> 
+							<FlatButton key={i} onClick = {this.clickAct.bind(this,i)}>{cont.name}</FlatButton> &emsp;&emsp;
+							<FloatingButton mini={true} onClick={this.editCnt.bind(this,i)}> <Edit/> </FloatingButton> &emsp;
+							<FloatingButton mini={true} backgroundColor="red" onClick={this.deleteCnt.bind(this,i)}> <ActionDelete/> </FloatingButton> <br/><br/>
+							{(this.state.show[i])?<p>Email: &emsp; {cont.email}<br/>Phone No: &emsp; {cont.no}</p>:null}
+							{(this.state.editId[i])?<editConatct edit={this.state.contacts}/>:null}
 						</li>)}
 				</ol>
 			</div>
 		)
 	}
+}
+
+function editContact(){
+
+	constructor(props){
+		
+		super(props);
+
+		var id = this.props.id;
+		this.state = {
+			name:this.props.contacts.name[id],
+			email:this.props.contacts.email[id],
+			no:this.props.contacts.no[id]
+		};
+	}
+
+	addCont(ev){
+		this.props.addContact(this.state);
+		ev.preventDefault();
+	}
+
+	valueChangeName(event){
+		this.setState({
+			name:event.target.value
+		})
+	}
+
+	valueChangeEmail(event){
+		this.setState({
+			email:event.target.value
+		})
+	}
+
+	valueChangeNo(event){
+		this.setState({
+			no:event.target.value
+		})
+	}
+
+	cancelEdit(){
+		
+	}
+
+	<form onSubmit={this.addCont.bind(this)}><br/>
+				Name <input type="text" name="name" value = {this.state.name[id]} onChange={this.valueChangeName.bind(this)} required/><br/><br/>
+				Email <input type="email" name="email" value = {this.state.email[id]} onChange={this.valueChangeEmail.bind(this)} /><br/><br/>
+				Mobile No. <input type="text" name="no" value = {this.state.no[id]} onChange={this.valueChangeNo.bind(this)}/><br/><br/>
+				<input type="submit" value="Save" /> <input type="button" value="Cancel" onClick={this.cancelEdit.bind(this)}/>
+			</form>
 }
 
 class NewContact extends React.Component{
@@ -125,14 +218,20 @@ class NewContact extends React.Component{
 
 	render(){
 		return(
-			<form onSubmit={this.addCont.bind(this)}>
+			<form onSubmit={this.addCont.bind(this)}><br/>
 				Name <input type="text" name="name" value = {this.state.name} onChange={this.valueChangeName.bind(this)} required/><br/><br/>
 				Email <input type="email" name="email" value = {this.state.email} onChange={this.valueChangeEmail.bind(this)} /><br/><br/>
 				Mobile No. <input type="text" name="no" value = {this.state.no} onChange={this.valueChangeNo.bind(this)}/><br/><br/>
 				<input type="submit" value="Add Contact" />
 			</form>
+
 		)
 	}
 }
 
+App.childContextTypes = {
+            muiTheme: React.PropTypes.object.isRequired,
+        };
+
 ReactDOM.render(<App />, document.getElementById('root'));
+
